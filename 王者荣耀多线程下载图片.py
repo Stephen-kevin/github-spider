be6ed16132bed5d610c2f4d57fdd48a85e2f0898
Stephen_kevin <1456225006>
2022-09-18 15:38:32 +0800

    第一次

--- 王者荣耀多线程下载图片.py
+++ 王者荣耀多线程下载图片.py
@@ -4,7 +4,7 @@ import threading
 from queue import Queue
 import os
 from urllib import request
-
+#王者壁纸的下载
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36',
     'referer': 'https://pvp.qq.com/web201605/wallpaper.shtml'  # 对于动态网页要加上referer，这个值是当前网页的网址
